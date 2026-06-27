ac86ceb9ae9da0f259413d1f9c8d98b9866249d6
AsimSudheer <asimsudheer512@gmail.com>
2026-06-27 22:05:02 +0530

    Time: 15 ms (42.96%) | Memory: 23.9 MB (27.12%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -7,7 +7,8 @@ class Solution:
             for c in s:
                 count[ord(c)-ord('a')] += 1
 
-            key =tuple(count)
+            key =tuple(count) #need to make it hashable so we convert it into tuple
             anagrams[key].append(s)
 
         return list(anagrams.values())
+#time: O(m.n)
